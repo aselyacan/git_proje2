0ab5945a69cdf8605157b679e3ac619477e8776b
aselyacan <iskender1982@gmail.com>
2020-06-21 21:05:24 +0200

    local reporsotry den ilk commit

---
+++ src/IkinciGun/SecondDay.java
@@ -0,0 +1,11 @@
+package IkinciGun;
+
+public class SecondDay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("First  connection to reporsotry ");
+	}
+
+}
